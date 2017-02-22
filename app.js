97f7eec7e5348d6506a2a4c31524d64a1dbc8f77
mkurapov <maxkurapov@gmail.com>
2017-02-21 22:16:34 -0700

    force login if result refreshed

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var request = require('request');
 var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 var port = process.env.PORT || 8080;
 var redirect_uri = port === 8080? 'http://192.168.1.74:8080/result' : 'https://music-signature.herokuapp.com/result';
 var client_id = process.env.CLIENT_ID;
 var client_secret = process.env.CLIENT_SECRET;
+var stateKey = 'spotify_auth_state';
 
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/login', (req, res) => {
+
+  var state = generateRandomString(16);
+  res.cookie('stateKey', state);
+
   var scope = 'user-library-read';
   res.redirect('https://accounts.spotify.com/authorize/?' +
       querystring.stringify({
@@ -24,29 +31,56 @@ app.get('/login', (req, res) => {
         redirect_uri: redirect_uri,
         response_type: 'code',
         scope: scope,
-        //show_dialog: true
+        state: state,
+       // show_dialog: true
       }));
 });
 
 app.get('/result', (req, res) => {
 
-  let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: req.query.code,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-    },
-    json: true
-  };
+  const code = req.query.code || null;
+  const storedCode = req.cookies.storedCode || null;
 
-  request.post(authOptions, (error, response, body) => {
-    res.render('result', {accessToken: body.access_token});
-  });
+
+  if (storedCode === code)
+  {
+    res.clearCookie('storedCode');
+    res.redirect('/login');
+  }
+  else {
+
+    res.cookie('storedCode', code);
+    let authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, (error, response, body) => {
+
+      const accessToken = body.access_token;
+      console.log('got access token: ' + accessToken);
+      res.cookie('access_token', accessToken)
+      res.render('result', {accessToken: accessToken});
+    });
+  }
 });
 
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
 app.listen(port, () => console.log('listening on: '+port));
